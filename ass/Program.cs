5b71ba460a28b14575c345862985626cdecbd957
Hung <hu6287834@gmail.com>
2026-01-22 14:26:07 +0700

    Implement multiply calculation

--- ass/Program.cs
+++ ass/Program.cs
@@ -21,6 +21,9 @@
             {
                 case SIGN.PLUS:
                     return arg1 + arg2;
+                case SIGN.MINUS:
+                    return arg1 - arg2;
+
                 case SIGN.MULTIPLY:
                     return arg1 * arg2;
 
