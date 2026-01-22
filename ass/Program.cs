87ef33998422064752990d4aff53cd8009231891
Hung <hu6287834@gmail.com>
2026-01-22 14:11:54 +0700

    Add full project including Program.cs and solution

---
+++ ass/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ass
+{
+    internal enum SIGN
+    {
+        PLUS,
+        MINUS,
+        MULTIPLY,
+        DIVIDE
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        private static double Calculate(SIGN sign, double arg1, double arg2)
+        {
+            switch (sign)
+            {
+                case SIGN.PLUS:
+                    return arg1 + arg2;
+
+
+                default:
+                    break;
+            }
+            return 0d;
+        }
+    }
+}
